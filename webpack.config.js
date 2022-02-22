95589e3d85b291427ed2a1eb5e86ab2d5e3006a9
L1nJIayu <562316920@qq.com>
2022-02-22 20:21:07 +0800

    config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin(),
+        new CleanWebpackPlugin()
+    ]
+}
